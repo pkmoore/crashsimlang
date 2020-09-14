e61d7a9c5620684722aa04e18df369ad9583b878
Preston Moore <preston@preston.dev>
2020-09-14 03:04:24 +0100

    Identify error cases, require identifiers, top_level types

--- datawords/adt.py
+++ datawords/adt.py
@@ -1,25 +1,50 @@
 from __future__ import print_function
+from cslang_error import CSlangError
 import pprint
-
 
 
 class ContainerBuilder(object):
   def __init__(self):
     self.builders = {"Int": self.primative, "String": self.primative}
+    self.top_level = {"Int": False, "String": False}
 
   def primative(self, in_data):
     incoming_type = in_data[0]
     incoming_arg = in_data[1]
-    return {"type": incoming_type, "argpos": incoming_arg, "members": []}
+    incoming_identifier = in_data[2]
+    return {"type": incoming_type, "argpos": incoming_arg, "members": [], "identifier": incoming_identifier}
 
   def define_type(self, container_type, types):
+    # It is an error for us to already have a builder for the type we're defining
+    if container_type in self.builders:
+      raise CSlangError("Illegal type redefinition for type: {}".format(container_type))
+
+    member_types = [t[0] for t in types]
+
+    # It is an error for types list to contain a type that has not been defined
+    for t in member_types:
+      if t not in self.builders:
+        raise CSlangError("Type definition contains undefined type: {}".format(t))
+
+    # Newly created types are considered top_level until used as a member type
+    self.top_level[container_type] = True
+
+    # If a type is used as a member in the type being defined, it is no longer
+    # a "top level" type for capturing purposes
+    for t in member_types:
+      self.top_level[t] = False
+
     member_builders = [self.builders[t[0]] for t in types]
 
-    c_type = container_type
-    def t_func(argv):
-      t = {"type": str(c_type)}
+    def t_func(in_data):
+      incoming_type = container_type
+      incoming_arg = in_data[1]
+      incoming_identifier = in_data[2]
+      t = {}
+      t["type"] = incoming_type
+      t["argpos"] = incoming_arg
+      t["identifier"] = incoming_identifier
       t["members"] = []
-      t["argpos"] = argv[1]
 
       # Step through each type and apply the appropriate builder
       # this takes our arguents and makes members out of them
@@ -41,6 +66,7 @@ class ContainerBuilder(object):
   def instantiate_type(self, t):
     instance = self.builders[t](t)
     del(instance["argpos"])
+    del(instance["identifier"])
     return instance
 
 
@@ -64,12 +90,14 @@ class ContainerBuilder(object):
 if __name__ == "__main__":
   pp = pprint.PrettyPrinter(indent=2)
   c = ContainerBuilder()
-  c.define_type("fakestat", [("Int", 0)])
-  c.define_type("statbuf", [("fakestat", 3), ("String", 0), ("String", 1), ("Int", 5)])
+  c.define_type("fakestat", [("Int", 0, "Somefield")])
+  c.define_type("statbuf", [("fakestat", 3, "Fakefield"), ("String", 0, "A_str"), ("String", 1, "Another_str"), ("Int", 5, "some_val")])
   tmp = c.instantiate_type("statbuf")
   pp.pprint(tmp)
   print()
   print()
-  c.define_type("stat", (("String", 0), ("statbuf", 1)))
+  c.define_type("stat", (("String", 0, "filename"), ("statbuf", 1, "stat_struct")))
   tmp = c.instantiate_type("stat")
+  c.define_type("open", (("Int", "ret", "filedesc"),))
+  tmp = c.instantiate_type("open")
   pp.pprint(tmp)

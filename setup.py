3abd3e8392f86ea8f47beca5beca8876f9402fe5
Almazhan Kapan <aa5456@nyu.edu>
2021-03-31 00:14:53 +0800

    run futurize stage 1,stage 2 on cslang

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ setup(
     author = "Preston Moore",
     author_email = "pkm266@nyu.edu",
     description = ("Compile and run cslang automata"),
-    url = "https://github.com/pkmoore/crashsimlang",
+    url = "https://github.com/almazhankapan/crashsimlang",
     install_requires = ["setuptools",
                         "ply",
                         "dill",
                         "lxml",
-                        "posix-omni-parser @ git+https://github.com/pkmoore/posix-omni-parser"],
+                        "posix-omni-parser @ git+https://github.com/almazhankapan/posix-omni-parser"],
     packages=['cslang'],
     entry_points = {
       "console_scripts": ['cslang = cslang.cslang:main']

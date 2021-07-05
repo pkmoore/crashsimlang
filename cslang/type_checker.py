13adf71abc33a593283caa063c6fcfb5968ac2b9
Hugo <zy1265@nyu.edu>
2021-07-05 09:08:21 -0400

    update the test for catching a structure that is trying to access ret position

--- cslang/type_checker.py
+++ cslang/type_checker.py
@@ -5,6 +5,7 @@ from .cslang_error import CSlangError
 
 def check_ast(ast):
     check_no_output_on_not_dataword(ast)
+    check_no_ret_in_none_top_level_event(ast)
 
 
 def check_no_output_on_not_dataword(ast):
@@ -15,3 +16,51 @@ def check_no_output_on_not_dataword(ast):
             if not_flag and node[5] != None:
                 print(node)
                 raise CSlangError("Output expressions are not allowed on NOT datawords")
+
+                
+
+#one of them this helper function
+#if one thing is called twice it is not top level
+def check_top_level_event(ast):
+    
+    top_level = {"Numeric": False, "String": False}    
+               
+    for node in ast:
+    
+    
+        #check if the node is defining a type   
+        if node[0] == "TYPEDEF":
+            node_event_type = node[1]
+            top_level[node_event_type] = True
+        
+            #if a type is used in an event, it is not a top level event
+            for member in node[2]:
+                top_level[member[0]] = False
+    
+
+            
+    return top_level    
+            
+
+
+
+
+def check_no_ret_in_none_top_level_event(ast):
+
+    #get the top level event list
+    top_level = check_top_level_event(ast)
+    print(top_level)
+    
+    for node in ast:
+        if node[0] == "TYPEDEF":   
+            node_event_type = node[1]
+            for member in node[2]:
+                if member[1] == 'ret':
+                    #check if a structure is accessing the value at ret position, which will raise an error
+                    if top_level[node_event_type] == False:
+                        print(node)
+                        raise CSlangError("A structure does not have return value position") 
+             
+            
+    
+
